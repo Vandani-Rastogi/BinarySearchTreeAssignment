6a14b2e32d87c5aafdc577f147593061b22f5978
Vandani-Rastogi <vandanidpd@gmail.com>
2021-04-13 23:32:11 +0530

    Main

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -13,6 +13,17 @@ public class MyMain {
         tree.insert(50);
         boolean result = tree.search(30);
         System.out.println("Element is present: "+result);
+        System.out.println("Inorder traversal of binary search tree is-");
+        tree.inOrder(tree.getRoot());
+        System.out.println();
+        System.out.println("Preorder traversal of binary search tree is-");
+        tree.preOrder(tree.getRoot());
+        System.out.println();
+        System.out.println("Postorder traversal of binary search tree is-");
+        tree.postOrder(tree.getRoot());
+        System.out.println();
+        System.out.println("Reversal Inorder traversal of binary search tree is-");
+        tree.reverseInOrder(tree.getRoot());
         //create binary search tree
         // and perform insert, search, traversal, delete
     }
