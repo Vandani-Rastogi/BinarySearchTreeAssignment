6edd2898fd5dcf4022c4475eaddb3991d236a572
Vandani-Rastogi <vandanidpd@gmail.com>
2021-04-13 21:30:34 +0530

    Insert method

--- src/implementation/MyBinarySearchTree.java
+++ src/implementation/MyBinarySearchTree.java
@@ -5,10 +5,42 @@ import myinterface.Node;
 
 public class MyBinarySearchTree<E extends Comparable<E>> implements BinarySearchTreeADT<E> {
     //complete this class
+    private implementation.Node<E> root;//head
 
+    public implementation.Node<E> getRoot() {
+        return root;
+    }
     @Override
     public void insert(E data) {
+    implementation.Node<E> node = new implementation.Node<>(data);
+        if (isEmpty()) {
+            root = node;
+        } else {
+            implementation.Node<E> temp = root;
+            implementation.Node<E> parent = null;
+            while (temp != null) {
+                parent = temp;
+                if (data.compareTo(temp.getData()) <= 0) {
+                    temp = temp.getLeft();
+                } else {
+                    temp = temp.getRight();
+                }
+            }
+            //identifying the position left or right
+            //to insert new node
+            if (data.compareTo(parent.getData()) <= 0) {
+                parent.setLeft(node);
+            } else {
+                parent.setRight(node);
+            }
+        }
+    }
 
+    private boolean isEmpty() {
+        if (root == null) {
+            return true;
+        }
+        return false;
     }
 
     @Override
--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -1,7 +1,14 @@
 package main;
 
+import implementation.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
+        MyBinarySearchTree<Integer> tree = new MyBinarySearchTree<Integer>();
+        tree.insert(10);
+        tree.insert(20);
+        tree.insert(30);
+        tree.insert(40);
         //create binary search tree
         // and perform insert, search, traversal, delete
     }
